d33174344fcd6fd882916b0d450823a314e151f7
Valentin Nicolas <valentin.ncls@gmail.com>
2017-03-23 22:52:45 +0100

    service request from webcam travel API

---
+++ app/js/controllers/webcamController.js
@@ -0,0 +1,6 @@
+angular.module('app')
+    .controller('webcamController', function($scope, dataService, $uibModal) {
+        dataService.getAll().then(function(res) {
+            $scope.webcam = res.data;
+});
+        });
---
+++ app/js/services/apiService.js
@@ -0,0 +1,11 @@
+var API = 'MZh5I1wwaZmshCtMV1y2RBVpyZMMp1OVME4jsnYuQUchPgUZy0';
+var myApp = angular.module('app')
+    .service('dataService', function($http) {
+        return {
+            getAll: function() {
+                return $http.get('https://webcamstravel.p.mashape.com/webcams/list/bbox=45.900,6.117,40.500,3.624?show=webcams:url&mashape-key=' + API);
+
+
+            }
+        };
+    });
