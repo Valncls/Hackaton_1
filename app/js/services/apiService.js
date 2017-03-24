8d7ad25649502d833f2a8fe0b8b8e81492c5b56e
maallard <mar.allard@gmail.com>
2017-03-24 11:01:42 +0100

    Réparation des markers

--- app/js/services/apiService.js
+++ app/js/services/apiService.js
@@ -7,7 +7,6 @@ var myApp = angular.module('app')
             },
             getAllInBox: function(neBound, swBound, zoom) {
                 return $http.get(`https://webcamstravel.p.mashape.com/webcams/map/${neBound.lat},${neBound.lng},${swBound.lat},${swBound.lng},10?show=webcams:url,location&mashape-key=${API}&limit=200,1`);
-                //return $http.get('https://webcamstravel.p.mashape.com/webcams/map/45.900,6.117,40.500,3.624,10?show=webcams:url,location&mashape-key=MZh5I1wwaZmshCtMV1y2RBVpyZMMp1OVME4jsnYuQUchPgUZy0')
             }
         };
     });
